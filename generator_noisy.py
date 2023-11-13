f242188dc7f7fa3f2589365bbead100a6a78e1b3
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2023-11-13 23:15:20 +0300

    Create generator_noisy.py

---
+++ generator_noisy.py
@@ -0,0 +1,133 @@
+import os
+import glob
+import tqdm
+import torch
+import random
+import librosa
+import argparse
+import numpy as np
+from multiprocessing import Pool, cpu_count
+import soundfile as sf
+from utils.audio import Audio
+from utils.hparams import HParam
+
+
+def formatter(dir_, form, num):
+    return os.path.join(dir_, form.replace('*', '%06d' % num))
+
+def vad_merge(w):
+    intervals = librosa.effects.split(w, top_db=20)
+    temp = list()
+    for s, e in intervals:
+        temp.append(w[s:e])
+    return np.concatenate(temp, axis=None)
+
+
+def mix(hp, args, audio, num, s1_dvec, s1_target, s2, noise, train):
+    srate = hp.audio.sample_rate
+    dir_ = os.path.join(args.out_dir, 'train' if train else 'test')
+
+    d, _ = librosa.load(s1_dvec, sr=srate)
+    w1, _ = librosa.load(s1_target, sr=srate)
+    w2, _ = librosa.load(s2, sr=srate)
+    noise, _ = librosa.load(noise, sr=srate)
+    assert len(d.shape) == len(w1.shape) == len(w2.shape) == 1, \
+        'wav files must be mono, not stereo'
+
+    d, _ = librosa.effects.trim(d, top_db=20)
+    w1, _ = librosa.effects.trim(w1, top_db=20)
+    w2, _ = librosa.effects.trim(w2, top_db=20)
+
+    # if reference for d-vector is too short, discard it
+    if d.shape[0] < 1.1 * hp.embedder.window * hp.audio.hop_length:
+        return
+
+    # LibriSpeech dataset have many silent interval, so let's vad-merge them
+    # VoiceFilter paper didn't do that. To test SDR in same way, don't vad-merge.
+    if args.vad == 1:
+        w1, w2 = vad_merge(w1), vad_merge(w2)
+
+    # I think random segment length will be better, but let's follow the paper first
+    # fit audio to `hp.data.audio_len` seconds.
+    # if merged audio is shorter than `L`, discard it
+    L = int(srate * hp.data.audio_len)
+    if w1.shape[0] < L or w2.shape[0] < L or noise.shape[0] < L:
+        return
+    w1, w2, noise = w1[:L], w2[:L], noise[:L]
+
+    mixed = w1 + w2 + noise
+
+    norm = np.max(np.abs(mixed)) * 1.1
+    w1, w2, mixed = w1/norm, w2/norm, mixed/norm
+
+    # save vad & normalized wav files
+    target_wav_path = formatter(dir_, hp.form.target.wav, num)
+    mixed_wav_path = formatter(dir_, hp.form.mixed.wav, num)
+    ref_wav_path = formatter(dir_, hp.form.reference.wav, num)
+
+
+    sf.write(target_wav_path, w1, srate)
+    sf.write(mixed_wav_path, mixed, srate)
+    sf.write(ref_wav_path, d, srate)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', type=str, required=True,
+                        help="yaml file for configuration")
+    parser.add_argument('-d', '--libri_dir', type=str, default=None,
+                        help="Directory of LibriSpeech dataset, containing folders of train-clean-100, train-clean-360, dev-clean.")
+    parser.add_argument('-v', '--voxceleb_dir', type=str, default=None,
+                        help="Directory of VoxCeleb2 dataset, ends with 'aac'")
+    parser.add_argument('-o', '--out_dir', type=str, required=True,
+                        help="Directory of output training triplet")
+    parser.add_argument('-p', '--process_num', type=int, default=None,
+                        help='number of processes to run. default: cpu_count')
+    parser.add_argument('--vad', type=int, default=0,
+                        help='apply vad to wav file. yes(1) or no(0, default)')
+    parser.add_argument('-n', '--noise_dir', type=str, required=True,
+                        help='Directory of WHAM noise dataset')
+    args = parser.parse_args()
+
+    os.makedirs(args.out_dir, exist_ok=True)
+    os.makedirs(os.path.join(args.out_dir, 'train'), exist_ok=True)
+    os.makedirs(os.path.join(args.out_dir, 'test'), exist_ok=True)
+
+    hp = HParam(args.config)
+
+    cpu_num = cpu_count() if args.process_num is None else args.process_num
+
+    if args.libri_dir is None and args.voxceleb_dir is None:
+        raise Exception("Please provide directory of data")
+
+    if args.libri_dir is not None:
+        test_folders = [x for x in glob.glob(os.path.join(args.libri_dir, 'dev-clean', '*'))]
+    
+    if args.noise_dir is not None:
+        noise_folders = [x for x in glob.glob(os.path.join(args.noise_dir, 'tr', '*'))]
+
+    elif args.voxceleb_dir is not None:
+        all_folders = [x for x in glob.glob(os.path.join(args.voxceleb_dir, '*'))
+                            if os.path.isdir(x)]
+        train_folders = all_folders[:-20]
+        test_folders = all_folders[-20:]
+
+    test_spk = [glob.glob(os.path.join(spk, '**', hp.form.input), recursive=True)
+                    for spk in test_folders]
+    test_spk = [x for x in test_spk if len(x) >= 2]
+
+
+    audio = Audio(hp)
+
+    def test_wrapper(num):
+        spk1, spk2 = random.sample(test_spk, 2)
+        noise = random.sample(noise_folders, 1)[0]
+        s1_dvec, s1_target = random.sample(spk1, 2)
+        s2 = random.choice(spk2)
+        mix(hp, args, audio, num, s1_dvec, s1_target, s2, noise, train=True)
+
+    arr = list(range(10**3))
+    with Pool(cpu_num) as p:
+        r = list(tqdm.tqdm(p.imap(test_wrapper, arr), total=len(arr)))
+
